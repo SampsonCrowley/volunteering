b664f2dd772665ea92e538f290a043c171249c51
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-20 14:13:26 -0700

    User

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+
+  before_save :format_input
+
+  has_secure_password
+
+  validates_uniqueness_of :email
+  validates_format_of :email, with: /\A[^@]+@[^@]+\.[^@]+\Z/, allow_nil: true
+  validates_format_of :password, with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{12,}\Z/, allow_nil: true
+
+  def regenerate_auth_token
+    destroy_token
+    generate_token
+    save!
+  end
+
+  def destroy_token
+    self.token = nil
+  end
+
+private
+
+    def format_input
+      email.downcase! if email
+    end
+
+    def generate_token
+      begin
+        self[:token] = SecureRandom.urlsafe_base64
+      end while User.exists?(token: self[:token])
+    end
+
+
+end
