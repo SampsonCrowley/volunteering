5cb740d0daf9bec498c9748865425946294a28aa
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-20 13:55:26 -0700

    add an update method

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -3,4 +3,9 @@ class StatesController < ApplicationController
   def index
     @results = StateStat.all.map {|state| [state.state, state.metrics]}
   end
+
+  def new
+    UpdateDataJob.perform_later
+    redirect_to :index
+  end
 end
