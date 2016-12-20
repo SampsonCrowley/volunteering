dba2e896c1fd28278256a69224fa82938de061b2
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-20 13:36:37 -0700

    test

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,13 @@
 require 'sidekiq/api'
 
 Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
   Rails.application.config.after_initialize do
     UpdateDataJob.perform_later if Sidekiq::ScheduledSet.new.none? {|scheduled| scheduled.queue == "update_data" }
   end
+end
+
+# so one sidekiq can have 7 connections
+Sidekiq.configure_server do |config|
+  config.redis = { :size => 7 }
 end
