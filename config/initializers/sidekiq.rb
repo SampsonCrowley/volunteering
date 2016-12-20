8cd45baa30ed24c2e855c63086857e647f3baaa0
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-20 13:49:10 -0700

    remove prestart

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,12 +2,12 @@ require 'sidekiq/api'
 
 Sidekiq.configure_client do |config|
   config.redis = { :size => 1 }
-  Rails.application.config.after_initialize do
-    UpdateDataJob.perform_later if Sidekiq::ScheduledSet.new.none? {|scheduled| scheduled.queue == "update_data" }
-  end
+  # Rails.application.config.after_initialize do
+  #   UpdateDataJob.perform_later if Sidekiq::ScheduledSet.new.none? {|scheduled| scheduled.queue == "update_data" }
+  # end
 end
 
-# so one sidekiq can have 7 connections
+# so one sidekiq can have 5 connections
 Sidekiq.configure_server do |config|
   config.redis = { :size => 5 }
 end
