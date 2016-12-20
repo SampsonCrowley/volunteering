6791e28ea30737a9690ab23b7f6680bbc56bc700
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-20 13:06:17 -0700

    remove auto worker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-require 'sidekiq/api'
-
-Sidekiq.configure_client do |config|
-  Rails.application.config.after_initialize do
-    UpdateDataJob.perform_later if Sidekiq::ScheduledSet.new.none? {|scheduled| scheduled.queue == "update_data" }
-  end
-end
+# require 'sidekiq/api'
+#
+# Sidekiq.configure_client do |config|
+#   Rails.application.config.after_initialize do
+#     UpdateDataJob.perform_later if Sidekiq::ScheduledSet.new.none? {|scheduled| scheduled.queue == "update_data" }
+#   end
+# end
